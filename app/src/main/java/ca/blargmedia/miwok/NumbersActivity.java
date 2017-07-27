4044554c281fee007736c2829edf73e100055844
blargmedia <blarg@blargmedia.ca>
2017-07-27 14:11:46 -0600

    added arraylist for words in numbers category

--- app/src/main/java/ca/blargmedia/miwok/NumbersActivity.java
+++ app/src/main/java/ca/blargmedia/miwok/NumbersActivity.java
@@ -2,6 +2,10 @@ package ca.blargmedia.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.activity_numbers);
+
+        for (int i = 0; i < words.size(); i++) {
+            // add textview to rootview
+            TextView tv = new TextView(this);
+            tv.setText(words.get(i));
+            rootView.addView(tv);
+        }
     }
 }
