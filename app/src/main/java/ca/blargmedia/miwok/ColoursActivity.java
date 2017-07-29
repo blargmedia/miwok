835f26b6074467f88af568455583748cade37593
blargmedia <blarg@blargmedia.ca>
2017-07-28 19:02:32 -0600

    added word lists for colours, family, and phrases

--- app/src/main/java/ca/blargmedia/miwok/ColoursActivity.java
+++ app/src/main/java/ca/blargmedia/miwok/ColoursActivity.java
@@ -2,6 +2,9 @@ package ca.blargmedia.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColoursActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ColoursActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colours);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        // use a listview for the number words
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list_colours);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
