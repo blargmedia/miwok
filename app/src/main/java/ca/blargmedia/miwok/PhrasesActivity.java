8393cffd5e22cdf0b9d6be91dd7baf5651443cbc
blargmedia <blarg@blargmedia.ca>
2017-08-13 20:22:59 -0600

    added audio to phrases

--- app/src/main/java/ca/blargmedia/miwok/PhrasesActivity.java
+++ app/src/main/java/ca/blargmedia/miwok/PhrasesActivity.java
@@ -1,29 +1,34 @@
 package ca.blargmedia.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
+    private MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "minto wuksus"));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә"));
-        words.add(new Word("My name is...", "oyaaset..."));
-        words.add(new Word("How are you feeling?", "michәksәs?"));
-        words.add(new Word("I’m feeling good.", "kuchi achit"));
-        words.add(new Word("Are you coming?", "әәnәs'aa?"));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
-        words.add(new Word("I’m coming.", "әәnәm"));
-        words.add(new Word("Let’s go.", "yoowutis"));
-        words.add(new Word("Come here.", "әnni'nem"));
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?", "minto wuksus", -1, R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinnә oyaase'nә", -1, R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", -1, R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michәksәs?", -1, R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I’m feeling good.", "kuchi achit", -1, R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "әәnәs'aa?", -1, R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", -1, R.raw.phrase_yes_im_coming));
+        words.add(new Word("I’m coming.", "әәnәm", -1, R.raw.phrase_im_coming));
+        words.add(new Word("Let’s go.", "yoowutis", -1, R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "әnni'nem", -1, R.raw.phrase_come_here));
 
         // use a listview for the number words
         WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_phrases);
@@ -31,6 +36,17 @@ public class PhrasesActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.word_list);
 
         listView.setAdapter(itemsAdapter);
+
+        // setup listener to play audio file
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // use the position to find the right audio file
+                mp = MediaPlayer.create(PhrasesActivity.this, words.get(position).getAudioResourceId());
+                mp.start();
+            }
+        });
 
     }
 }
