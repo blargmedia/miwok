47da32cae060fb80f66474942ce9e5df83fe18ff
blargmedia <blarg@blargmedia.ca>
2017-08-22 18:29:55 -0600

    modified ui to use ViewPager and Fragments instead of Activities and Intents

---
+++ app/src/main/java/ca/blargmedia/miwok/CategoryAdapter.java
@@ -0,0 +1,35 @@
+package ca.blargmedia.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * CategoryAdapter
+ */
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColoursFragment();
+            default:
+                return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/ca/blargmedia/miwok/MainActivity.java
+++ app/src/main/java/ca/blargmedia/miwok/MainActivity.java
@@ -15,11 +15,9 @@
  */
 package ca.blargmedia.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,51 +28,14 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Handling click events for numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // create a listener inline
-            @Override
-            public void onClick(View v) {
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Handling click events for colours category
-        TextView colours = (TextView) findViewById(R.id.colors);
-        colours.setOnClickListener(new View.OnClickListener() {
-            // create a listener inline
-            @Override
-            public void onClick(View v) {
-                Intent coloursIntent = new Intent(MainActivity.this, ColoursActivity.class);
-                startActivity(coloursIntent);
-            }
-        });
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
-        // Handling click events for family category
-        TextView family = (TextView) findViewById(R.id.family);
-        family.setOnClickListener(new View.OnClickListener() {
-            // create a listener inline
-            @Override
-            public void onClick(View v) {
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
-
-        // Handling click events for phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            // create a listener inline
-            @Override
-            public void onClick(View v) {
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
-
-
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
     }
 }
